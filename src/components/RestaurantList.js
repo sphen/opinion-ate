a96921cff579bc29e711773a05efd0e9a648bdd8
sphen <sphen@users.noreply.github.com>
2021-05-14 14:58:28 -0600

    Load restaurants upon first rendering RestaurantList

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,7 +1,15 @@
+import {useEffect} from 'react';
 // export as default and named
 // default will be connected to redux
 // named export for testing
-export const RestaurantList = () => {
+export const RestaurantList = ({loadRestaurants}) => {
+  // useEffect to run loadRestaurants each time it changes
+  // it shouldn't change so it should just run once
+  // when component renders
+  useEffect(() => {
+    loadRestaurants();
+  }, [loadRestaurants]);
+
   return <div>RestaurantList</div>;
 };
 
