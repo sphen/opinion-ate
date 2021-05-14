3e2c1c11ee86d33cb4936c7debc51b1ea48b9544
sphen <sphen@users.noreply.github.com>
2021-05-14 15:14:01 -0600

    Display restaurants

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -2,7 +2,7 @@ import {useEffect} from 'react';
 // export as default and named
 // default will be connected to redux
 // named export for testing
-export const RestaurantList = ({loadRestaurants}) => {
+export const RestaurantList = ({loadRestaurants, restaurants}) => {
   // useEffect to run loadRestaurants each time it changes
   // it shouldn't change so it should just run once
   // when component renders
@@ -10,7 +10,13 @@ export const RestaurantList = ({loadRestaurants}) => {
     loadRestaurants();
   }, [loadRestaurants]);
 
-  return <div>RestaurantList</div>;
+  return (
+    <ul>
+      {restaurants.map(restaurant => (
+        <li key={restaurant.id}>{restaurant.name}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default RestaurantList;
--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -2,15 +2,43 @@ import {render} from '@testing-library/react';
 import {RestaurantList} from '../RestaurantList';
 
 describe('RestaurantList', () => {
+  // test if restaurants load on first render
   it('loads restaurants on first render', () => {
     // Jest.fn() creats a mock function to allow check
     // that loadRestaurants action was called
     // chain mockName() to it to give function name
     const loadRestaurants = jest.fn().mockName('loadRestaurants');
+    // empty array for restaurants since we don't care about displaying them
+    const restaurants = [];
     // import RestaurantList component, render it,
     // and pass loadRestaurants as a component prop
-    render(<RestaurantList loadRestaurants={loadRestaurants} />);
+    render(
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />,
+    );
     // check that mock function was called and run expecation
     expect(loadRestaurants).toHaveBeenCalled();
+  });
+
+  // test if restaurants display
+  it('displays the restaurants', () => {
+    // noop = no operation
+    const noop = () => {};
+    // define restaurants var
+    const restaurants = [
+      {id: 1, name: 'Sushi Place'},
+      {id: 2, name: 'Pizza Place'},
+    ];
+    // destructured queryByText function to check what is rendered
+    const {queryByText} = render(
+      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />,
+    );
+    // queryByText finds an element containing the passed-in text.
+    // If found, queryByText returns a reference to the element; if not found, it returns null
+    // to confirm they are found, we check that return result is not null.
+    expect(queryByText('Sushi Place')).not.toBeNull();
+    expect(queryByText('Pizza Place')).not.toBeNull();
   });
 });
